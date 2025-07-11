fb0b46287d8b68ad448d0c65e6a664b8117a7591
Loto-Kenneth <116004412+Loto-Kenneth@users.noreply.github.com>
2025-07-11 11:17:05 +0800

    Established Connection in Supabase, Added uuid type tp id column, and Added one sample user

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::factory()->create([
+            'name' => 'Kenesu',
+            'email' => 'kenesu@example.com',
+            'password' => 1234567890,
+        ]);
+    }
+}
